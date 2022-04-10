46e03657784a7f248977170dd654476e4daf9d19
impleotv <alexc@impleotv.com>
2022-04-10 08:57:49 +0300

    dev

--- index.js
+++ index.js
@@ -35,4 +35,16 @@ function generateReadMe() {
   });
 }
 
+function generateVersionsFile() {
+  fs.readFile(MUSTACHE_MAIN, (err, data) =>  {
+    if (err) throw err;
+    const output = {
+      latest: pjsonServer.version,
+      date: new Date().toISOString()
+    }
+    fs.writeFileSync('versions.json', output);
+  });
+}
+
 generateReadMe();
+generateVersionsFile();
